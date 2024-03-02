7df158f279c07e94ad1c445eb16f6877b570fa2a
Muhammad Fahimul Huq <124661681+fahimECOM@users.noreply.github.com>
2024-03-02 19:49:41 +0600

    first commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <Navbar />
+</template>
+
+
+<script>
+import Navbar from '~/components/Header.vue'
+
+export default{
+  components:{
+    Navbar
+  }
+}
+
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="bg-blue text-white py-5 mx-auto">
+    <div class="w-[80%] flex justify-between mx-auto">
+        <div class="text-xl font-bold">Navbar</div>
+        <div class="flex gap-10">
+            <div class=" flex gap-2 align-middle font-light  hover:text-green cursor-pointer">Portfolio <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+</svg>
+</div>
+            <div class="flex gap-2 align-middle  font-light   hover:text-green cursor-pointer">About <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+</svg>
+</div>
+            <div class="flex gap-2 align-middle font-light  hover:text-green cursor-pointer">Contact <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+</svg>
+</div>
+        </div>
+    </div>
+       
+    </div>
+</template>
